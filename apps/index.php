8f84cd7cd97567ecd621a3b8910b9c8302f0f4aa
A-lone <aloooomail@gmail.com>
2014-03-23 17:01:33 +0800

    onepiece

--- apps/index.php
+++ apps/index.php
@@ -1,3 +1,8 @@
-<?php
-//phpinfo();
-echo "hi there: welcome to my site.";
+<html>
+<head>
+	<title>Welcome</title>
+</head>
+<body>
+	<iframe marginwidth=0 marginheight=0 width='100%'height='100%' frameborder=0 src="http://tv.sohu.com/s2013/onepiece/"></iframe>
+</body>
+</html>
